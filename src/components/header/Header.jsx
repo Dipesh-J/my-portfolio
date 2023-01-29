5f1b552ebb5aa61c90bc596ea15b65b84e30668c
Dipesh-J <dipeshjoshi015@gmail.com>
2023-01-30 02:29:18 +0530

    kuch to bna aaj

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './header.css'
+const Header = () => {
+  return (
+    <div>
+        <h4> Hi I'm </h4>
+        <h1> Dipesh Joshi </h1>
+        <p> The luckiest man alive..❤</p>
+    </div>
+  )
+}
+
+export default Header
