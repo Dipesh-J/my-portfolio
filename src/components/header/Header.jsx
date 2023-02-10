48fdf053f6fd21e0599fbf3696560d330960f2c1
Dipesh-J <dipeshjoshi015@gmail.com>
2023-02-11 00:11:37 +0530

    day 2 complete

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
 const Header = () => {
   return (
-    <div>
-        <h4> Hi I'm </h4>
-        <h1> Dipesh Joshi </h1>
-        <p> The luckiest man alive..❤</p>
-    </div>
+    // <div>
+    //     <h4> Hi I'm </h4>
+    //     <h1> Dipesh Joshi </h1>
+    //     <p> The luckiest man alive..❤</p>
+    // </div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm </h5>
+        <h1>Dipesh Joshi</h1>
+        <h5 className="text-light">Full Stack Developer</h5>
+        <CTA/>
+      </div>
+    </header>
   )
 }
 
