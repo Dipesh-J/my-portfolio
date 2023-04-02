b6256410709f49f86728eaa71359c2de5efb02f3
Dipesh-J <dipeshjoshi015@gmail.com>
2023-04-02 18:48:18 +0530

    style changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,27 @@
-import React from 'react'
+import React from "react";
 
-import Header from './components/header/Header'
-import Nav from './components/nav/Nav';
-import About from './components/about/About';
-import Experience from './components/experience/Experience';
-import Testimonials from './components/testimonials/Testimonials';
-import Projects from './components/projects/Projects';
-import Portfolio from './components/portfolio/Portfolio'
-import Contact from './components/contact/Contact';
-import Footer from './components/footer/Footer';
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import About from "./components/about/About";
+import Experience from "./components/experience/Experience";
+import Projects from "./components/projects/Projects";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
   return (
     <>
-      <Nav/>
-      <br/>
-      <Header/>
-      <br/>
-      <About/>
-      <Experience/>
-      <Projects/>
-      <Portfolio/>
-      <Testimonials/>
-      <Contact/>
-      <Footer/>
+      <Nav />
+      <br />
+      <Header />
+      <br />
+      <About />
+      <Experience />
+      <Projects />
+      <Contact />
+      <Footer />
     </>
+  );
+};
 
-    )
-}
-
-export default App
+export default App;
